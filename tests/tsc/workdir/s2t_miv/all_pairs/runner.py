160177359d1e5b33b49a1aa379b9d6fafa4f0032
ArmelliniMG <giuliana_armellini@hotmail.com>
2020-04-07 12:08:48 +0200

    small test changes

--- tests/tsc/workdir/s2t_miv/all_pairs/runner.py
+++ tests/tsc/workdir/s2t_miv/all_pairs/runner.py
@@ -3,4 +3,4 @@ import subprocess
 import tscdefs
 
 os.chdir("data")
-subprocess.call(tscdefs.get_python_tool("s2t_miv.py",None) + ['--real-trips', 'vehroutes_oneshot_meso.rou.xml', '--representatives', 'miv_all_pairs_passenger24.trips.rou.alt.xml', '-n', 'scenario_workdir/mitte_net/net.net.xml'])
+subprocess.call(tscdefs.get_python_tool("s2t_miv.py") + ['--real-trips', 'vehroutes_oneshot_meso.rou.xml', '--representatives', 'miv_all_pairs_passenger24.trips.rou.alt.xml', '-n', 'scenario_workdir/mitte_net/net.net.xml'])
