eeb0534c8cdff1012e737e699ab086c7488796fb
ArmelliniMG <giuliana_armellini@hotmail.com>
2020-04-14 19:08:28 +0200

    updated test

--- tests/tsc/template/tscDaemonDatabaseTripsFromFile/runner.py
+++ tests/tsc/template/tscDaemonDatabaseTripsFromFile/runner.py
@@ -5,4 +5,4 @@ import db_manipulator
 updateCmd = "INSERT INTO core.global_sumo_status VALUES('2015y_01m_19d_15h_55m_15s_386ms', 0, NOW(), 'disabled', 'error');"
 
 os.chdir("data")
-db_manipulator.start(tscdefs.testServer, tscdefs.get_python_tool("tsc_main.py") + ['--fake-tripfile', 'twoPersonsOnly.csv', '-l', '2', '--limit', '5000', '--sim-param', 'SUMO_TEMPLATE_FOLDER:mitte_net,SUMO_DESTINATION_FOLDER:mitte_net,DB_TABLE_ADDITIONAL_TRAFFIC:'], [open("initialState.sql"), [updateCmd]], [], [])
+db_manipulator.start(tscdefs.testServer, tscdefs.get_python_tool("tsc_main.py") + ['--fake-tripfile', 'twoPersonsOnly.csv', '-l', '2', '--limit', '50000', '--sim-param', 'SUMO_TEMPLATE_FOLDER:mitte_net,SUMO_DESTINATION_FOLDER:mitte_net,DB_TABLE_ADDITIONAL_TRAFFIC:'], [open("initialState.sql"), [updateCmd]], [], [])
