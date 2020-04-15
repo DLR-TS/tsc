d30bddee54062ce78a4b23a764e4fdd518b988a5
ArmelliniMG <giuliana_armellini@hotmail.com>
2020-04-15 12:46:27 +0200

    changed sim_key for test

--- tests/tsc/template/tscDaemonDatabaseTripsFromDB/runner.py
+++ tests/tsc/template/tscDaemonDatabaseTripsFromDB/runner.py
@@ -3,10 +3,10 @@ import tscdefs
 import subprocess
 import db_manipulator
 
-updateCmd = ["UPDATE public.simulation_parameters SET sim_key='2015y_01m_19d_15h_55m_15s_386ms';",
+updateCmd = ["UPDATE public.simulation_parameters SET sim_key='2015y_05m_29d_09h_01m_11s_943ms';",
              "UPDATE public.simulation_parameters SET param_value='berlin_trips' WHERE param_key='DB_TABLE_TRIPS';",
              "UPDATE public.simulation_parameters SET param_value='1' WHERE param_key='SUMO_MAX_ITERATION';",
-             "UPDATE core.global_sumo_status SET sim_key='2015y_01m_19d_15h_55m_15s_386ms';"]
+             "UPDATE core.global_sumo_status SET sim_key='2015y_05m_29d_09h_01m_11s_943ms';"]
 
 os.chdir("data")
-db_manipulator.start(tscdefs.testServer, tscdefs.get_python_tool("tsc_main.py") + ['--limit', '5000'], [open('initialState.sql'), updateCmd], [], [])
+db_manipulator.start(tscdefs.testServer, tscdefs.get_python_tool("tsc_main.py") + ['--limit', '50000'], [open('initialState.sql'), updateCmd], [], [])
