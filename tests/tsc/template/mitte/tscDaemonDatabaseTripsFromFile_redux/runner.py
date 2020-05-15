c88597f950cbfc609ea2225bc01712fad643ea7a
ArmelliniMG <giuliana_armellini@hotmail.com>
2020-05-15 21:02:46 +0200

    fixing tests

--- tests/tsc/template/mitte/tscDaemonDatabaseTripsFromFile_redux/runner.py
+++ tests/tsc/template/mitte/tscDaemonDatabaseTripsFromFile_redux/runner.py
@@ -7,9 +7,9 @@ updateCmd = """UPDATE simulation_parameters SET param_value='mitte_net'
                WHERE param_key='SUMO_DESTINATION_FOLDER' OR param_key='SUMO_TEMPLATE_FOLDER';"""
 
 pre_path = os.path.join(tscdefs.tscData, "data")
-templates_path = os.path.join("data", "data_templates")
+templates_path = os.path.join("data", "scenario_templates")
 
 subprocess.call(tscdefs.get_python_tool("install_scenario_templates.py") + ['-s', 'mitte_net', '-p', pre_path, '-t', templates_path])
 
 os.chdir("data")
-db_manipulator.start(tscdefs.testServer, tscdefs.get_python_tool("tsc_main.py") + ['--fake-tripfile', 'twoPersonsOnly.csv', '-l', '2', '--limit', '5000'], [open('initialState.sql'), [updateCmd]], [], [])
+db_manipulator.start(tscdefs.testServer, tscdefs.get_python_tool("tsc_main.py") + ['--fake-tripfile', 'twoPersonsOnly.csv', '-l', '2', '--limit', '50000'], [open('initialState.sql'), [updateCmd]], [], [])
