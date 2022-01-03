6952f994c66356f797132a9ce7cd4e2bce8763b2
Michael Behrisch <michael.behrisch@dlr.de>
2022-01-03 15:37:05 +0100

    adapting tests

---
+++ tests/tsc/template/tscDaemonVTypeMatrix/runner.py
@@ -0,0 +1,12 @@
+import os
+import datetime
+import tscdefs
+import db_manipulator
+
+updateCmd = "INSERT INTO public.global_sumo_status VALUES('2015y_01m_19d_15h_55m_15s_386ms', 0, '%s', 'disabled', 'error');" % datetime.datetime.now().isoformat()
+
+os.chdir("data")
+db_manipulator.start(tscdefs.testServer,
+                     tscdefs.get_python_tool("tsc_main.py") + ['--fake-tripfile', 'twoPersonsOnly.csv', '-l', '2', '--limit', '50000', '--vtype-matrix',
+                                                               '--sim-param', 'SUMO_TEMPLATE_FOLDER:mitte_net,SUMO_DESTINATION_FOLDER:mitte_net,DB_TABLE_ADDITIONAL_TRAFFIC:'],
+                     [open("initialState.sql"), [updateCmd]], [], [])
