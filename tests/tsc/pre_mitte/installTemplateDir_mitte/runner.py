aebb544ca102048e7112df8a04c0a62c428ccb3b
Michael Behrisch <oss@behrisch.de>
2022-12-19 18:10:11 +0100

    adapting test setup

--- tests/tsc/pre_mitte/installTemplateDir_mitte/runner.py
+++ tests/tsc/pre_mitte/installTemplateDir_mitte/runner.py
@@ -2,4 +2,4 @@ import os
 import subprocess
 import tscdefs
 
-subprocess.call(tscdefs.get_python_tool("install_scenario_templates.py") + ['--clean', '--suburb-taz', '',  '-v', '-p', os.path.join(tscdefs.tscRoot, 'data')])
+subprocess.call(tscdefs.get_python_tool("install_scenario_templates.py") + ['--clean', '--suburb-taz', '',  '-v', '-p', os.path.join(tscdefs.tscRoot, 'data'), '-s', 'mitte_test'])
